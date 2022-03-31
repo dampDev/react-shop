d5d9a3765d2a226c8f80e36b1c94898090668731
daniel <dmendezp@live.com.mx>
2022-03-31 08:57:44 -0600

    page checkout

--- src/containers/RecoveryPassword.jsx
+++ src/containers/RecoveryPassword.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import '../styles/RecoveryPassword.scss';
 const RecoveryPassword = () => {
     return (
-        <div className="login">
-    <div className="form-container">
+        <div className="CreateAccount">
+    <div className="CreateAccount-container">
       <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
       <h1 className="title">Email has been sent!</h1>
       <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,8 @@ import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
 import '../styles/global.css';
 
 const App =() => {
@@ -15,6 +17,9 @@ const App =() => {
             <Routes>
             <Route exact path="/" element={<Home/>} />
                 <Route exact path="/login" element={<Login/>} />
+                <Route exact path="/create-account" element={<CreateAccount/>} />
+                <Route exact path="/checkout" element={<Checkout/>} />
+                
                 <Route exact path="/recovery-password" element={<RecoveryPassword/>}/>
                 <Route exact path="*" element={<NotFound/>}/>
                 </Routes>
